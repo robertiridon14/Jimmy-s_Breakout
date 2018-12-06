d9bd5146124888bfb461ec98d96937d186853928
daescucristi03 <37413650+daescucristi03@users.noreply.github.com>
2018-12-06 19:04:00 +0200

    Add files via upload

---
+++ DirectorEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectorEntity : MonoBehaviour {
+
+
+    [SerializeField] private Vector3 Epsilon;
+    private struct ArieInteres
+    {
+        float North;
+        float South;
+        float West;
+        float East;
+    }
+
+
+    private Vector3 getPlayerPosition()
+    {
+        return transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        Debug.Log(getPlayerPosition());
+    }
+}
