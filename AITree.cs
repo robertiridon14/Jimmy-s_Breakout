c86150f87960ea8a089032c3bde24bd26d3c89ac
daescucristi03 <37413650+daescucristi03@users.noreply.github.com>
2018-12-06 21:37:05 +0200

    Add files via upload

---
+++ AITree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AITree : MonoBehaviour {
+
+    public Transform moveSpots;
+    public float speed;
+
+    private float WaitTime;
+    public float startWaitTime;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+	void Awake()
+    {
+        startWaitTime = startWaitTime;
+        moveSpots.position = new Vector3(Random.RandomRange(minX, maxX), Random.RandomRange(0, 5), Random.RandomRange(minY, maxY));
+    }
+	
+
+	void Update () {
+            transform.position = Vector3.MoveTowards(transform.position,moveSpots.position, speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position,moveSpots.position) < 0.2f)
+        {
+            if(WaitTime <= 0)
+            {
+                WaitTime = startWaitTime;
+                moveSpots.position = new Vector3(Random.RandomRange(minX, maxX), Random.RandomRange(0, 5), Random.RandomRange(minY, maxY));
+            }
+            else
+            {
+                WaitTime -= Time.deltaTime;
+            }
+        }
+
+    }
+}
