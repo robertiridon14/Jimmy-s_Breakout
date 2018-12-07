864153be50963b879f5525116488a7422bf6a376
robertiridon14 <robertiridon@gmail.com>
2018-12-07 23:38:10 +0200

    Update DirectorEntity.cs

--- DirectorEntity.cs
+++ DirectorEntity.cs
@@ -1,46 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class DirectorEntity : MonoBehaviour {
-
-
-    [SerializeField] private Vector3 Epsilon;
-    [SerializeField] private float Omega;
-
-        private float North;
-        private float South;
-        private float West;
-        private float East;
-        private float Up;
-        private float Down;
-    
-    private void Awake()
+using DirectorAI;
+namespace DirectorAI
+{
+    public class DirectorEntity : MonoBehaviour
     {
-        Omega = 25.0f;
-        Epsilon.x = Omega;
-        Epsilon.z = Omega;
-        Epsilon.y = Omega;
-    }
-    private void getPlayerPosition()
-    {
-        North = transform.position.x + Epsilon.x;
-        Debug.Log("nord " + North);
-        South = transform.position.x - Epsilon.x;
-        Debug.Log("sud " + South);
-        East = transform.position.z + Epsilon.z;
-        Debug.Log("est " + East);
-        West = transform.position.z - Epsilon.z;
-        Debug.Log("vest " + West);
-        Up = transform.position.y + Epsilon.y;
-        Debug.Log("up " + Up);
-        Down = transform.position.y - Epsilon.y;
-        Debug.Log("Down " + Down);
-    }
+        [SerializeField] private Vector3 Epsilon;
+        [SerializeField] private float Omega;
 
-    
-    void Update () {
+        public float North;
+        public float South;
+        public float West;
+        public float East;
+        public float Up;
+        public float Down;
 
-        getPlayerPosition();
+        private void Awake()
+        {
+            Omega = 25.0f;
+            Epsilon.x = Omega;
+            Epsilon.z = Omega;
+            Epsilon.y = Omega;
+        }
+
+        private void getPlayerPosition()
+        {
+            North = transform.position.x + Epsilon.x;
+            Debug.Log("nord " + North);
+            South = transform.position.x - Epsilon.x;
+            Debug.Log("sud " + South);
+            East = transform.position.z + Epsilon.z;
+            Debug.Log("est " + East);
+            West = transform.position.z - Epsilon.z;
+            Debug.Log("vest " + West);
+            Up = transform.position.y + Epsilon.y;
+            Debug.Log("up " + Up);
+            Down = transform.position.y - Epsilon.y;
+            Debug.Log("Down " + Down);
+        }
     }
 }
